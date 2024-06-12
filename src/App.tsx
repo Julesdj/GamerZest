03e102190df18cb75c888d84359c0cc7559b7f82
Julesdj <julesdjog@gmail.com>
2024-06-12 11:42:00 -0400

    Implements navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
     return (
         <>
-            <nav className="w-full bg-blue-500 mb-4">Navbar</nav>
+            <NavBar />
             <div className="flex gap-4 w-full min-h-svh">
                 <aside className="hidden lg:block w-full max-w-52 bg-red-500">
                     Sidebar
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+function NavBar() {
+    return (
+        <nav className="w-full h-16 flex gap-4 items-center mb-4">
+            <div>
+                <span className="font-bold">GAMERZEST</span>
+                <span className="sr-only">Logo</span>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
