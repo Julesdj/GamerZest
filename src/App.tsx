bc6510af9ad5ff1c707bd1d5101744e3aee747de
Julesdj <julesdjog@gmail.com>
2024-06-12 10:40:13 -0400

    Updates the theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
     return (
         <>
-            <h1>Hello World</h1>
+            <Button>Button</Button>
         </>
     );
 }
