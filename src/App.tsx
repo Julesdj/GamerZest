11838538bdf602e11f8100d7a7b96a93070d8bb2
Julesdj <julesdjog@gmail.com>
2024-06-12 12:39:11 -0400

    Implements the mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
     return (
         <>
             <NavBar />
-            <div className="flex gap-4 w-full min-h-svh">
-                <aside className="hidden lg:block w-full max-w-52 bg-red-500">
+            <div className="flex gap-4 w-full min-h-svh container">
+                <aside className="hidden lg:block w-full max-w-[300px] bg-white">
                     Sidebar
                 </aside>
                 <main className="w-full flex-1 bg-black">Main</main>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,13 @@
+import ThemeModeToggle from "./ThemeModeToggle";
+
 function NavBar() {
     return (
-        <nav className="w-full h-16 flex gap-4 items-center mb-4">
+        <nav className="container w-full h-16 flex justify-between gap-4 items-center mb-4">
             <div>
                 <span className="font-bold">GAMERZEST</span>
                 <span className="sr-only">Logo</span>
             </div>
+            <ThemeModeToggle />
         </nav>
     );
 }
