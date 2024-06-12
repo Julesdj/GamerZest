8a1b07b17b0a3939c356804ecd670b5259ef9278
Julesdj <julesdjog@gmail.com>
2024-06-12 11:27:22 -0400

    Implements responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
 
 function App() {
     return (
         <>
-            <Button>Button</Button>
+            <nav className="w-full bg-blue-500 mb-4">Navbar</nav>
+            <div className="flex gap-4 w-full min-h-svh">
+                <aside className="hidden lg:block w-full max-w-52 bg-red-500">
+                    Sidebar
+                </aside>
+                <main className="w-full flex-1 bg-black">Main</main>
+            </div>
         </>
     );
 }
