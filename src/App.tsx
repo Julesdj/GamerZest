db9ee3937f554bb89dc65c3fbb04e69bc94f76fd
Julesdj <julesdjog@gmail.com>
2024-06-12 21:52:49 -0400

    Implements games fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import GameGrid from "./components/GameGrid";
 import NavBar from "./components/NavBar";
 
 function App() {
     return (
         <>
             <NavBar />
-            <div className="flex gap-4 w-full min-h-svh container">
-                <aside className="hidden lg:block w-full max-w-[300px] bg-white">
-                    Sidebar
-                </aside>
-                <main className="w-full flex-1 bg-black">Main</main>
+            <div className="flex gap-4 w-full min-h-[95vh] container">
+                <aside className="hidden lg:block w-60 bg-white">Sidebar</aside>
+                <main className="w-full flex-1 p-2">
+                    <GameGrid />
+                </main>
             </div>
         </>
     );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import ThemeModeToggle from "./ThemeModeToggle";
 
 function NavBar() {
     return (
-        <nav className="container w-full h-16 flex justify-between gap-4 items-center mb-4">
+        <nav className="container w-full h-16 shadow-md backdrop-blur-sm bg-card/90 flex justify-between gap-4 items-center mb-4 2xl:my-4 rounded-md">
             <div>
                 <span className="font-bold">GAMERZEST</span>
                 <span className="sr-only">Logo</span>
